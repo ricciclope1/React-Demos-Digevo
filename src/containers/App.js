dff2d597c3ff314636b6ff9e2ad690500c3235ea
Alejandro Vivanco <manuelinviv@gmail.com>
2020-09-04 16:03:47 -0500

    cambio en la descripcion

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,8 @@ function App() {
               title='COCO-SSD'
               description='This model detects objects defined in the COCO dataset, 
               which is a large-scale object detection, segmentation, and captioning dataset. 
-              The model is capable of detecting 80 classes of objects. (SSD stands for Single Shot MultiBox Detection).'
+              The model is capable of detecting 80 classes of objects. (SSD stands for Single Shot MultiBox Detection).
+              Please allow the use of pop-up windows in the browser for the correct functioning of the application.'
             />
             <HOCCoco />
           </Layout>
@@ -26,5 +27,3 @@ function App() {
   );
 }
 export default App;
-/*
-            <Coco />*/
