8c20cfb110d21697efb0e39c43eff500da053de1
Thalía Zapata <thaliazapata@Thalias-MacBook-Pro.local>
2020-09-04 18:31:39 -0500

    responsive

--- src/styles/CocoContainer.js
+++ src/styles/CocoContainer.js
@@ -1,41 +1,147 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CocoDiv = styled.div`
   padding: 3em;
   height: 38em;
+  position: relative;
+  @media only screen and (max-width: 1200px) {
+    position: relative;
+    height: 45em;
+  }
+  @media only screen and (max-width: 850px) {
+    position: relative;
+    height: 40em;
+  }
+  @media only screen and (max-width: 576px) {
+    position: relative;
+    height: 27em;
+  }
 `;
 export const Main = styled.div`
   position: absolute;
-  left: 25%;
+  top: 50%;
+  left: 45%;
   transform: translate(-50%, -50%);
+  box-sizing: content-box;
+  width: 46%;
+  height: 75%;
+
+  @media only screen and (max-width: 1200px) {
+    position: absolute;
+    top: -12%;
+    left: 50%;
+    transform: translate(-50%, 50%);
+    box-sizing: content-box;
+    width: 80%;
+    height: 65%;
+  }
+  @media only screen and (max-width: 850px) {
+    position: absolute;
+    top: -3%;
+    left: 50%;
+    transform: translate(-50%, 50%);
+    box-sizing: content-box;
+    width: 80%;
+    height: 55%;
+  }
+  @media only screen and (max-width: 576px) {
+    position: absolute;
+    top: 5%;
+    left: 50%;
+    transform: translate(-50%, 50%);
+    box-sizing: content-box;
+    width: 80%;
+    height: 43%;
+  }
 `;
 export const Video = styled.video`
   position: fixed;
+  box-sizing: content-box;
+  width: 100%;
+  height: 100%;
+  border-radius: 1em;
+
+  @media only screen and (max-width: 1200px) {
+    box-sizing: content-box;
+    width: 100%;
+    height: 100%;
+    border-radius: 1em;
+  }
+  @media only screen and (max-width: 850px) {
+    box-sizing: content-box;
+    width: 100%;
+    height: 100%;
+    border-radius: 1em;
+  }
+  @media only screen and (max-width: 576px) {
+    box-sizing: content-box;
+    width: 100%;
+    height: 100%;
+    border-radius: 1em;
+  }
 `;
 export const Canvas = styled.canvas`
   position: fixed;
   border-style: ridge;
   border-radius: 0.5em;
+  box-sizing: content-box;
+  width: 100%;
+  height: 95%;
   -webkit-box-shadow: 15px 15px 3px 1px rgb(0, 0, 0);
   -moz-box-shadow: 15px 15px 3px 1px rgb(6, 7, 7);
   box-shadow: 15px 15px 3px 1px rgb(17, 17, 17);
+  @media only screen and (max-width: 1200px) {
+    box-sizing: content-box;
+    width: 100%;
+    height: 95%;
+  }
+  @media only screen and (max-width: 850px) {
+    box-sizing: content-box;
+    width: 100%;
+    height: 95%;
+  }
+  @media only screen and (max-width: 576px) {
+    box-sizing: content-box;
+    width: 100%;
+    height: 95%;
+  }
 `;
 export const MainGraphic = styled.div`
   display: flex;
   justify-content: center;
+  height: 45em;
+  @media only screen and (max-width: 576px) {
+    display: none;
+  }
 `;
 export const Graphic = styled.div`
   //padding: 2em 0em 2em 20%;
 `;
 export const Play = styled.button`
-  border: solid 1px black;
-  height: 3em;
-  margin: 20% 1em 0em 1em;
+  background: transparent;
+  border: 2px solid #0099cc;
+  border-radius: 6px;
+  height: 2.5em;
+  margin: 15% 1em 0em 1em;
+  @media only screen and (max-width: 1200px) {
+    margin: 25% 1em 0em 1em;
+  }
+  @media only screen and (max-width: 850px) {
+    margin: 30% 1em 0em 1em;
+  }
 `;
 export const Stop = styled.button`
-  border: solid 1px black;
-  height: 3em;
-  margin: 20% 1em 0em 1em;
+  background: transparent;
+  border: 2px solid #0099cc;
+  border-radius: 6px;
+  height: 2.5em;
+  margin: 15% 1em 0em 1em;
+  @media only screen and (max-width: 1200px) {
+    margin: 25% 1em 0em 1em;
+  }
+  @media only screen and (max-width: 850px) {
+    margin: 30% 1em 0em 1em;
+  }
 `;
 export const MessageContainer = styled.div`
   display: flex;
--- src/styles/Demos.js
+++ src/styles/Demos.js
@@ -1,41 +1,44 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Principal = styled.div`
-  font-family: 'VremenaGroteskRegular', sans-serif;
+  font-family: "VremenaGroteskRegular", sans-serif;
   background-color: #f8f8f8;
   height: 30%;
   display: flex;
 
-  @media only screen and (max-width: 500px) {
+  @media only screen and (max-width: 1200px) {
+    display: flex;
+    height: 60%;
+    padding: 0.8em;
+  }
+  @media only screen and (max-width: 850px) {
     display: block;
     height: 40%;
+    padding: 1em;
+  }
+  @media only screen and (max-width: 576px) {
+    display: block;
+    height: 35%;
+    padding: 1em;
   }
 `;
 export const Icon = styled.div`
   padding-left: 6em;
   animation: animacion 0.4s 3;
   font-size: large;
-  .ic{
+  .ic {
     margin-right: 1em;
   }
   a {
     text-decoration: none;
     margin-right: 1em;
-  
   }
-  @media only screen and (max-width: 500px) {
-    display: block;
-    height: 35%;
-    padding-left: 1em;
-}
-  @media only screen and (max-width: 768px) {
-    display: block;
-    height: 40%;
-    padding-left: 1em;
-    a {
-        margin: 0.5em 3em;
-        font-size: 1em;
-    }
+  @media only screen and (max-width: 850px) {
+    margin: -0.5em -1em;
+  }
+  @media only screen and (max-width: 576px) {
+    margin: -1em 0.5em;
+  }
 `;
 
 export const Global = styled.div`
@@ -48,28 +51,43 @@ export const Global = styled.div`
   }
 
   @keyframes animacion {
-  0% {
-    color: #000;
-    transform: translateY(15%);
-  }
-  33% {
-    color: rgb(13, 224, 183);
-    transform: translateY(-15px);
-  }
-  66% {
-    color: rgb(106, 27, 154);
-    transform: translateY(-15px);
-  }
-  100% {
-    color: rgb(36, 103, 217);
-    transform: translateY(15px);
-  }
-}
-  @media only screen and (max-width: 768px) {
-    h1{
-        margin: 2em 2.3em;
-        font-size: 1.2em;
+    0% {
+      color: #000;
+      transform: translateY(15%);
     }
+    33% {
+      color: rgb(13, 224, 183);
+      transform: translateY(-15px);
+    }
+    66% {
+      color: rgb(106, 27, 154);
+      transform: translateY(-15px);
+    }
+    100% {
+      color: rgb(36, 103, 217);
+      transform: translateY(15px);
+    }
+  }
+  @media only screen and (max-width: 1200px) {
+    h1 {
+      width: 70%;
+      margin-top: 20%;
+    }
+  }
+  @media only screen and (max-width: 850px) {
+    margin: -4.2em -2em;
+    padding-top: 1em;
+    h1 {
+      margin-top: 30%;
+      width: 100%;
+    }
+  }
+  @media only screen and (max-width: 576px) {
+    margin: -4em -3.5em;
+    h1 {
+      margin-top: 60%;
+    }
+  }
 `;
 
 export const Description = styled.div`
@@ -83,8 +101,23 @@ export const Description = styled.div`
     font-size: 1.1em;
     text-align: justify;
   }
-  @media only screen and (max-width: 768px) {
-    p{
-        font-size: 1em;
+  @media only screen and (max-width: 1200px) {
+    margin: 3em 3em;
+    width: 70%;
+    overflow: auto;
+  }
+  @media only screen and (max-width: 850px) {
+    margin: 6em 3em;
+    width: 75%;
+    overflow: auto;
+
+    p {
+      font-size: 1em;
     }
+  }
+  @media only screen and (max-width: 576px) {
+    margin: 5em 3em;
+    width: 65%;
+    overflow: auto;
+  }
 `;
